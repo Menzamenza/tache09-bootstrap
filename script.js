e34de19a83f8eaa41621d93b40223b815e39dc8b
Menza <menzamenza90@gmail.com>
2023-12-17 00:52:05 +0000

    1st finish

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-
+// popover cart shopping
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
